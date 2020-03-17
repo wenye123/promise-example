3b2b4ea7c085c29d6e2ac08ad0fba832ff2d7c41
wenye <1583247975@qq.com>
2020-03-17 17:41:32 +0800

    feat(index.ts): 初步完成

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,109 @@
-export function getStr() {
-  return "hello";
+export type IPromiseExecFn = (resolve: IPromiseResove, reject: IPromiseReject) => any;
+export type IPromiseResolveFn = (res: any) => any;
+export type IPromiseRejectFn = (reason: any) => any;
+export type IPromiseResove = (res: any) => void;
+export type IPromiseReject = (reason: any) => void;
+export type IIPromiseState = "pending" | "resolved" | "rejected";
+export interface IPromiseTask {
+  resolveCb: IPromiseResolveFn | undefined;
+  rejectCb: IPromiseRejectFn | undefined;
+  resolve: IPromiseResove;
+  reject: IPromiseReject;
 }
+
+export class Promise {
+  private tasks: IPromiseTask[] = [];
+  private value: any = null;
+  private state: IIPromiseState = "pending";
+
+  /** 构造函数 */
+  constructor(execFn: IPromiseExecFn) {
+    try {
+      execFn(this.resolve, this.reject);
+    } catch (e) {
+      this.reject(e);
+    }
+  }
+
+  /** 采用箭头函数是为了绑定this */
+  private resolve = (ret: any) => {
+    if (this.state === "pending") {
+      // resolve的值为IPromise实例
+      if (ret && (typeof ret === "object" || typeof ret === "function")) {
+        // 为了兼容其他IPromise实现
+        const then = ret.then;
+        if (typeof then === "function") {
+          then.call(ret, this.resolve, this.reject);
+          return;
+        }
+      }
+      // resolve的值为非IPromise实例
+      this.value = ret;
+      this.state = "resolved";
+      this.execute();
+    }
+  };
+
+  /** 采用箭头函数是为了绑定this */
+  private reject = (reason: any) => {
+    if (this.state === "pending") {
+      this.value = reason;
+      this.state = "rejected";
+      this.execute();
+    }
+  };
+
+  /** 收集then任务并返回新的Promise */
+  then(resolveCb?: IPromiseResolveFn, rejectCb?: IPromiseRejectFn) {
+    return new Promise((resolve, reject) => {
+      this.handleTask({
+        resolveCb,
+        rejectCb,
+        resolve,
+        reject,
+      });
+    });
+  }
+
+  /** 处理任务 */
+  private handleTask(task: IPromiseTask) {
+    // 尚未resolve/reject时推入任务数组
+    if (this.state === "pending") {
+      this.tasks.push(task);
+      return;
+    }
+    // resolve/reject时执行cb回调
+    let cb = this.state === "resolved" ? task.resolveCb : task.rejectCb;
+    if (cb === undefined) {
+      cb = this.state === "resolved" ? task.resolve : task.reject;
+      cb(this.value);
+      return;
+    }
+    try {
+      const ret = cb(this.value);
+      task.resolve(ret);
+    } catch (err) {
+      task.reject(err);
+    }
+  }
+
+  /** 循环执行promise的回调 */
+  private execute() {
+    setTimeout(() => {
+      this.tasks.forEach(task => {
+        this.handleTask(task);
+      });
+    }, 0);
+  }
+}
+
+/** 测试出口 */
+// (Promise as any).defer = (Promise as any).deferred = function() {
+//   const dfd: any = {};
+//   dfd.promise = new Promise((resolve, reject) => {
+//     dfd.resolve = resolve;
+//     dfd.reject = reject;
+//   });
+//   return dfd;
+// };
+// module.exports = Promise;
--- test/test-index.ts
+++ test/test-index.ts
@@ -1,8 +1,62 @@
-import { getStr } from "../src/index";
+import { Promise } from "../src/index";
 import { assert } from "chai";
 
-describe("test", function() {
-  it("hello", function() {
-    assert.equal(getStr(), "hello");
+function getPromise(str: string) {
+  return new Promise(resolve => {
+    setTimeout(function() {
+      resolve(str);
+    }, 50);
+  });
+}
+
+describe("Promise", function() {
+  it("execFn err", function(done) {
+    new Promise((resolve, reject) => {
+      throw new Error("err");
+    }).then(
+      r => {},
+      e => {
+        assert.strictEqual(e.message, "err");
+        done();
+      },
+    );
+  });
+
+  it("reject", function(done) {
+    new Promise((resolve, reject) => {
+      reject(new Error("err"));
+    }).then(
+      r => {},
+      e => {
+        assert.strictEqual(e.message, "err");
+        done();
+      },
+    );
+  });
+
+  it("resolve & then reject", function(done) {
+    getPromise("wenye")
+      .then(r => {
+        assert.strictEqual(r, "wenye");
+        throw new Error("err");
+      })
+      .then(
+        r => {},
+        e => {
+          assert.strictEqual(e.message, "err");
+          done();
+        },
+      );
+  });
+
+  it("return promise", function(done) {
+    getPromise("wenye")
+      .then(r => {
+        return getPromise("yiye");
+      })
+      .then(r => {
+        assert.strictEqual(r, "yiye");
+        done();
+      });
   });
 });
